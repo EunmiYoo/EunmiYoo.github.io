fb56641814fba0b008723e421e29b3e3872dc028
eyoo <eyoo@Eunmiui-MacBookPro.local>
2023-11-30 01:35:05 +0100

    tile change

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Snow-crash Level00",
+        "excerpt":"1. First, connect to ssh with your ip address : ssh -i ~/.ssh/id_rsa.pub level00@165.22.17.96 -p 4242 you will see… _____ _____ _ / ____| / ____| | | | (___ _ __ _____ _| | _ __ __ _ ___| |__ \\___ \\| '_ \\ / _ \\ \\ /\\...","categories": [],
+        "tags": ["CyberSecurity","Github","Git"],
+        "url": "/level00/",
+        "teaser": null
+      }]
